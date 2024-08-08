c10638a02753b4478a7829277b2069d540b171b7
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-08-08 21:38:00 +0800

    chore(deps): bump all deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 // @ts-check
-import vida from '@vida0905/eslint-config'
+import { defineConfig } from '@vida0905/eslint-config'
 import nuxt from './.nuxt/eslint.config.mjs'
 
 export default nuxt()
   .prepend(
-    vida({
+    defineConfig({
       unocss: true,
       formatters: true,
     }),
