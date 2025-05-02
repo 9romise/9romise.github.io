48cd95ef44cab8ba5a02a8b3ebed27dc0609f4ec
Vida Xie <vida_2020@163.com>
2025-05-03 00:56:41 +0800

    fix: highlight code

--- components/content/ProsePre.vue
+++ components/content/ProsePre.vue
@@ -1,5 +1,15 @@
+<script lang="ts" setup>
+defineOptions({
+  inheritAttrs: false,
+})
+</script>
+
 <template>
-  <div class="overflow-auto rounded-2 bg-gray-100 p-1em dark:bg-gray-800">
-    <pre><slot /></pre>
+  <div
+    class="overflow-auto rounded-2 bg-gray-100 p-1em dark:bg-gray-800"
+  >
+    <pre
+      v-bind="$attrs"
+    ><slot /></pre>
   </div>
 </template>
