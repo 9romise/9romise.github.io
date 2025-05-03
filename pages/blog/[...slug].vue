a35d62bd43c14f8d786134b9642f35ae800b6289
Vida Xie <vida_2020@163.com>
2025-05-03 15:32:19 +0800

    fix: try fix hydration

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -13,15 +13,22 @@ useSeoMeta({
 </script>
 
 <template>
-  <main v-if="page">
+  <main>
     <article class="px-0 lg:px-20">
-      <h1 class="mb-4 mt-0 text-10 text-gray-900 dark:text-gray-100">
-        {{ page.title }}
-      </h1>
-      <p class="mt--6 text-gray-500 dark:c-gray-400">
-        {{ `${dayjs(page.date).format('MMM DD, YYYY ddd')}.` }}
-      </p>
-      <ContentRenderer class="text-gray-600 dark:c-gray-300" :value="page" />
+      <template v-if="page">
+        <h1 class="mb-4 mt-0 text-10 text-gray-900 dark:text-gray-100">
+          {{ page.title }}
+        </h1>
+        <p class="mt--6 text-gray-500 dark:c-gray-400">
+          {{ `${dayjs(page.date).format('MMM DD, YYYY ddd')}.` }}
+        </p>
+        <ContentRenderer class="text-gray-600 dark:c-gray-300" :value="page" />
+      </template>
+      <template v-else>
+        <p class="text-gray-500 dark:c-gray-400">
+          Loading...
+        </p>
+      </template>
     </article>
   </main>
 </template>
