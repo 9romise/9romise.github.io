7c92a5b2f1f8799125b87229c153064a3d0103d2
Vida Xie <vida_2020@163.com>
2025-07-16 10:27:28 +0800

    chore: update deps, lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,6 @@
-import nuxt from '@nuxt/eslint-plugin'
-import { defineConfig, GLOB_SRC } from '@vida0905/eslint-config'
+import { defineConfig } from '@vida0905/eslint-config'
 
 export default defineConfig({
   unocss: true,
   formatters: true,
-}, {
-  files: [GLOB_SRC],
-  plugins: {
-    nuxt,
-  },
-  rules: {
-    'nuxt/nuxt-config-keys-order': 'error',
-    'nuxt/prefer-import-meta': 'error',
-  },
 })
